f1a818464672ecf4ece89bca1841f866b68b100f
JennyYu2 <jennyyu@email.arizona.edu>
2025-05-29 14:04:39 -0700

    updated the dark/light mode switch

--- js/index.js
+++ js/index.js
@@ -12,120 +12,26 @@ function getTheme() {
 
 
 function setDark() {
-    // general theme
-    var body = document.body
-    if(body) {
-        body.classList.add("dark")
-    }
-
-    var navname = document.querySelector(".navname")
-    if(navname) {
-        navname.classList.add("dark")
-    }
-
-    var navLinks = document.querySelectorAll(".navli a")
-    for(var i = 0; i < navLinks.length; i++) {
-        navLinks[i].classList.add("dark")
-    }
-
-    var navbar = document.querySelectorAll(".navbar a")
-    for(var i = 0; i < navbar.length; i++) {
-        navbar[i].classList.add("dark")
-    }
-
-    var themeBtn = document.getElementById("theme")
-    if(themeBtn) {
-        themeBtn.classList.add("dark")
-    }
-
-    var footer = document.querySelector("footer")
-    if(footer) {
-        footer.classList.add("dark")
-    }
-
-    var footerLinks = document.querySelectorAll("footer a")
-    for(var i = 0; i < footerLinks.length; i++) {
-        footerLinks[i].classList.add("dark")
-    }
-
-    // homepage theme
-    var bio = document.getElementById("bio")
-    bio.classList.add("dark")
-
-    var contactLinks = document.querySelectorAll(".contact-links a")
-    for (var i = 0; i < contactLinks.length; i++) {
-        contactLinks[i].classList.add("dark")
-    }
-
-    var contact = document.querySelectorAll(".contact")
-    for (var i = 0; i < contact.length; i++) {
-        contact[i].classList.add("dark")
-    }
+    document.documentElement.classList.add("dark")
 
     // change to sun
     var icon = document.querySelector("#theme i")
     icon.classList.remove("fa-moon")
     icon.classList.add("fa-sun")
 
+    // add theme to storage
     localStorage.setItem("theme", "dark")
 }
 
 function setLight() {
-    // general theme
-    var body = document.body
-    if(body) {
-        body.classList.remove("dark")
-    }
-
-    var navname = document.querySelector(".navname")
-    if(navname) {
-        navname.classList.remove("dark")
-    }
-
-    var navLinks = document.querySelectorAll(".navli a")
-    for(var i = 0; i < navLinks.length; i++) {
-        navLinks[i].classList.remove("dark")
-    }
-
-    var navbar = document.querySelectorAll(".navbar a")
-    for(var i = 0; i < navbar.length; i++) {
-        navbar[i].classList.remove("dark")
-    }
-
-    var themeBtn = document.getElementById("theme")
-    if(themeBtn) {
-        themeBtn.classList.remove("dark")
-    }
-
-    var footer = document.querySelector("footer")
-    if(footer) {
-        footer.classList.remove("dark")
-    }
-
-    var footerLinks = document.querySelectorAll("footer a")
-    for(var i = 0; i < footerLinks.length; i++) {
-        footerLinks[i].classList.remove("dark")
-    }
-
-    // homepage theme
-    var bio = document.getElementById("bio")
-    bio.classList.remove("dark")
-
-    var contactLinks = document.querySelectorAll(".contact-links a")
-    for (var i = 0; i < contactLinks.length; i++) {
-        contactLinks[i].classList.remove("dark")
-    }
-
-    var contact = document.querySelectorAll(".contact")
-    for (var i = 0; i < contact.length; i++) {
-        contact[i].classList.remove("dark")
-    }
+    document.documentElement.classList.remove("dark")
 
     // change to moon
     var icon = document.querySelector("#theme i")
     icon.classList.remove("fa-sun")
     icon.classList.add("fa-moon")
 
+    // add theme to storage
     localStorage.setItem("theme", "light")
 }
 
--- js/projects.js
+++ js/projects.js
@@ -25,138 +25,26 @@ function getTheme() {
 
 
 function setDark() {
-
-
-    // general theme
-    var body = document.body
-    if(body) {
-        body.classList.add("dark")
-    }
-
-    var navname = document.querySelector(".navname")
-    if(navname) {
-        navname.classList.add("dark")
-    }
-
-    var navLinks = document.querySelectorAll(".navli a")
-    for(var i = 0; i < navLinks.length; i++) {
-        navLinks[i].classList.add("dark")
-    }
-
-    var navbar = document.querySelectorAll(".navbar a")
-    for(var i = 0; i < navbar.length; i++) {
-        navbar[i].classList.add("dark")
-    }
-
-    var themeBtn = document.getElementById("theme")
-    if(themeBtn) {
-        themeBtn.classList.add("dark")
-    }
-
-    var footer = document.querySelector("footer")
-    if(footer) {
-        footer.classList.add("dark")
-    }
-
-    var footerLinks = document.querySelectorAll("footer a")
-    for(var i = 0; i < footerLinks.length; i++) {
-        footerLinks[i].classList.add("dark")
-    }
-
-    // projects theme
-    var heading = document.querySelector("h2")
-    heading.classList.add("dark")
-
-    var block = document.querySelectorAll(".block")
-    for(var i = 0; i < block.length; i++) {
-        block[i].classList.add("dark")
-    }
-
-    var blockA = document.querySelectorAll(".block a")
-    for(var i = 0; i < blockA.length; i++) {
-        blockA[i].classList.add("dark")
-    } 
-
-    var content = document.querySelectorAll(".content")
-    for(var i = 0; i < content.length; i++) {
-        content[i].classList.add("dark")
-    }
+    document.documentElement.classList.add("dark")
 
     // change to sun
     var icon = document.querySelector("#theme i")
-    if (icon) {
-        icon.classList.remove("fa-moon")
-        icon.classList.add("fa-sun")
-    }
+    icon.classList.remove("fa-moon")
+    icon.classList.add("fa-sun")
 
+    // add theme to storage
     localStorage.setItem("theme", "dark")
 }
 
 function setLight() {
-
-    // general theme
-    var body = document.body
-    if(body) {
-        body.classList.remove("dark")
-    }
-
-    var navname = document.querySelector(".navname")
-    if(navname) {
-        navname.classList.remove("dark")
-    }
-
-    var navLinks = document.querySelectorAll(".navli a")
-    for(var i = 0; i < navLinks.length; i++) {
-        navLinks[i].classList.remove("dark")
-    }
-
-    var navbar = document.querySelectorAll(".navbar a")
-    for(var i = 0; i < navbar.length; i++) {
-        navbar[i].classList.remove("dark")
-    }
-
-    var themeBtn = document.getElementById("theme")
-    if(themeBtn) {
-        themeBtn.classList.remove("dark")
-    }
-
-    var footer = document.querySelector("footer")
-    if(footer) {
-        footer.classList.remove("dark")
-    }
-
-    var footerLinks = document.querySelectorAll("footer a")
-    for(var i = 0; i < footerLinks.length; i++) {
-        footerLinks[i].classList.remove("dark")
-    }
-
-    // projects theme
-    var heading = document.querySelector("h2")
-    heading.classList.remove("dark")
-
-    var block = document.querySelectorAll(".block")
-    for(var i = 0; i < block.length; i++) {
-        block[i].classList.remove("dark")
-    }
-
-    var blockA = document.querySelectorAll(".block a")
-    for(var i = 0; i < blockA.length; i++) {
-        blockA[i].classList.remove("dark")
-    } 
-
-    var content = document.querySelectorAll(".content")
-    for(var i = 0; i < content.length; i++) {
-        content[i].classList.remove("dark")
-    }
-
+    document.documentElement.classList.remove("dark")
 
     // change to moon
     var icon = document.querySelector("#theme i")
-    if (icon) {
-        icon.classList.remove("fa-sun")
-        icon.classList.add("fa-moon")
-    }
+    icon.classList.remove("fa-sun")
+    icon.classList.add("fa-moon")
 
+    // add theme to storage
     localStorage.setItem("theme", "light")
 }
 
@@ -173,3 +61,6 @@ function changeTheme() {
         setDark()
     }
 }
+
+
+// TODO: back to the top button
